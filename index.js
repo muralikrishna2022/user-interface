a6c654e24ef8668c0fd312c4ed449f4fa2be7376
muralikrishna2022 <muralikrishnach3999@gmail.com>
2023-06-13 18:23:20 -0400

    updating

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const userRoutes = require('./server/routes/user');
+
+mongoose.connect(process.env.dbURL)
+  .then(console.log("DB Connected!!"))
+  .catch(error => console.log(error));
+
+app.use(express.json());
+
+app.use(express.static(__dirname + "/client/build"));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/client/build', 'index.html')));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin,X-Requested-Width,Content-Type,Accept,Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+  next();
+});
+
+app.use('/user', userRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
